0104fe94033aad8ba8ca022bec0bf2448f0f4db1
Anatolii Bondarenko <anatbond4@gmail.com>
2024-12-24 12:06:44 +0200

    Resolved task "Save the Prisoner"

---
+++ src/algorithms/SavePrisoner.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+import java.io.*;
+import java.util.stream.*;
+
+public class SavePrisoner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = saveThePrisoner(n, m, s);
+                System.out.println(result);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        int remind = (m + s - 1) % n;
+        return remind > 0 ? remind : n;
+    }
+
+}
