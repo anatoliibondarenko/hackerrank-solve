e5b52084b0b63d82868e276a2a228286b97b38dc
Anatolii Bondarenko <anatbond4@gmail.com>
2024-12-27 13:25:40 +0200

    Resolved task "Jumping on the clouds" and "Duplicate words".

---
+++ src/algorithms/JumpingOnTheClouds.java
@@ -0,0 +1,56 @@
+package algorithms;
+
+import java.io.*;
+import java.util.*;
+
+public class JumpingOnTheClouds {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+        int n = c.length;
+        int i = 0;
+        int e = 100;
+
+        do {
+            i = (i + k) % n;
+            if (c[i] == 0) {
+                e--;
+            } else {
+                e -= 3;
+            }
+        } while (i != 0);
+
+        return e;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
