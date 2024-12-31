db567b0fe2705bf8fec76cdfd4a86085105bbf12
Anatolii Bondarenko <anatbond4@gmail.com>
2024-12-31 08:57:08 +0200

    Resolved task "CamelCase".

---
+++ src/algorithms/CamelCase.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+import java.io.*;
+
+public class CamelCase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = camelcase(s);
+        System.out.println(result);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static int camelcase(String s) {
+        return (int) s.chars().filter(ch -> ch <= 'Z').count() + 1;
+    }
+}
